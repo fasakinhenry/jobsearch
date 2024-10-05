e3c7c959972082872ef9afe7c12d9ae62a5f062d
fisayo-dev <olufisayobadina@gmail.com>
2024-10-05 19:54:31 +0100

    working on the search bar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,35 @@
+import { LightBulbIcon } from "@heroicons/react/24/outline";
 const Hero = () => {
   return (
     <div className="py-5 h-full">
       <div className="container px-20">
-        <div className="grid justify-items-start place-items-start">Hero</div>
+        <div className="grid  place-items-start">
+          <div className="grid py-6 max-w-5xl mx-auto">
+            <div className="text-center grid gap-8">
+              <h2 className="text-6xl font-extrabold">
+                Job Opportunities at Your Fingertips.
+              </h2>
+              <p className="text-md">
+                Navigate Your Future: Your Job Search Made Simple!
+              </p>
+            </div>
+            <div className="py-5">
+              <div className="items-center flex w-4/5 mx-auto py-2 px-3 rounded-full overflow-hidden  bg-white shadow-md">
+                <div className="px-3">
+                  <LightBulbIcon className="h-6 w-6" />
+                </div>
+                <input
+                  type="text"
+                  placeholder="Search for jobs"
+                  className="w-4/5 px-3"
+                />
+                <div className="w-1/5 text-center btn-color px-3 py-4 rounded-full">
+                  Search now
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/App/landing.jsx
+++ src/pages/App/landing.jsx
@@ -1,7 +1,7 @@
 import { Header, Hero } from "../../components";
 const Landing = () => {
   return (
-    <div className="blue h-[100vh] w-[100vw] overflow-hidden">
+    <div className="bg-pattern blue h-[100vh] w-[100vw] overflow-hidden">
       <div className="grid ">
         <Header />
         <Hero />
