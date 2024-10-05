5eb16dbe60065bc9f6fc2256653a5f8e7ff5694d
fisayo-dev <olufisayobadina@gmail.com>
2024-10-05 18:57:34 +0100

    modified header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Button from "./Button";
 
 const Header = () => {
   return (
@@ -6,10 +7,23 @@ const Header = () => {
       <div className="px-20 container">
         <div className="flex items-center justify-between">
           <h2 className="text-3xl font-bold">Job Search</h2>
+          <div className="flex justify-center items-center gap-5">
+            <Link to="/" className="text-md">
+              Home
+            </Link>
+            <Link to="/search" className="text-md">
+              Search
+            </Link>
+            <Link to="/about" className="text-md">
+              About
+            </Link>
+            <Link to="/careers" className="text-md">
+              Careers
+            </Link>
+          </div>
           <div className="flex items-center gap-3">
-            <Link to="/" className="text-md">Home</Link>
-            <Link to="/" className="text-md">Search</Link>
-            <Link to="/" className="text-md">Careers</Link>
+            <Button filled={true}>Sign Up</Button>
+            <Button filled={false}>Login</Button>
           </div>
         </div>
       </div>
