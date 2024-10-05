622e7a0d43e4c9c271fe7cfb7aadcd160a9133fe
fisayo-dev <olufisayobadina@gmail.com>
2024-10-05 17:48:01 +0100

    routed landing page to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import {
+  Router,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import { Landing } from "./pages/App";
+import NotFound from "./pages";
+
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Router path="/" element={<Landing />} />
+        <Router path="*" element={<NotFound />} />
+      </>
+    )
+  );
+  return <RouterProvider router={router} />;
 }
