96d22ebc23f557658aec13387c8e6252e11ca214
fisayo-dev <olufisayobadina@gmail.com>
2024-10-05 18:00:34 +0100

    fixed routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import {
-  Router,
+  Route,
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
 import { Landing } from "./pages/App";
-import {NotFound} from "./pages";
+import { NotFound } from "./pages";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Router path="/" element={<Landing />} />
-        <Router path="*" element={<NotFound />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
       </>
     )
   );
