9157dbecd176b2a0e284491a8d47b48b55ece2b6
fisayo-dev <olufisayobadina@gmail.com>
2024-10-05 19:06:19 +0100

    lading page update: header available, hero under progress

--- src/pages/App/landing.jsx
+++ src/pages/App/landing.jsx
@@ -1,8 +1,11 @@
-import {Header} from "../../components";
+import { Header, Hero } from "../../components";
 const Landing = () => {
   return (
-    <div>
-      <Header />
+    <div className="blue grid h-[100vh] w-[100vw] overflow-hidden">
+      <div className="grid justify-content-start">
+        <Header />
+        <Hero />
+      </div>
     </div>
   );
 };
