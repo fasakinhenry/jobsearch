e94a738714328b47d107d738f19c7c9f0104ea98
fisayo-dev <olufisayobadina@gmail.com>
2024-10-05 17:38:46 +0100

    created landing page

---
+++ src/pages/App/landing.jsx
@@ -0,0 +1,7 @@
+const Landing = () => {
+  return (
+    <div>Landing</div>
+  )
+}
+
+export default Landing
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+const NotFound = () => {
+  return (
+    <div className="text-center text-3xl font-bold">This page is not found</div>
+  )
+}
+
+export default NotFound
