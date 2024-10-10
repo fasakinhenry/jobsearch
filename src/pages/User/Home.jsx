2a1201ab1ac07a60d6ec833fc21f74f82d3fd009
fisayo-dev <olufisayobadina@gmail.com>
2024-10-10 23:18:31 +0100

    tweaked and added some designs to the job cards

--- src/pages/User/Home.jsx
+++ src/pages/User/Home.jsx
@@ -101,7 +101,7 @@ const Home = () => {
         <div className="grid mt-5">
           <div className="grid grid-cols-1 md:grid-cols-4 2xl:grid-cols-5 gap-5 px-1 py-1">
             {jobs.map((job) => (
-              <div key={job.id} className="grid bg-gray-100 shadow-md gap-4 rounded-xl px-4 py-3 overflow-hidden">
+              <div key={job.id} className="grid hover:scale-105 hover:transition cursor-pointer bg-gray-100 shadow-md gap-4 rounded-xl px-4 py-3 overflow-hidden">
                 <div className="flex gap-3 items-center">
                   <img src={avatar} className="h-8 w-8 rounded-full" />
                   <h2 className="text-[1rem] w-full">
@@ -126,11 +126,12 @@ const Home = () => {
                   </h2>
                   <p>{job.type}</p>
                 </div>
-                <div className="text-[0.9rem]">
+                <div className="text-[0.9rem] flex items-center justify-between">
                   <Button styles="bg-green-500 hover:bg-green-600">
                     <ShareIcon className="h-6 w-6" />
                     <p>Share</p>
                   </Button>
+                  <p className="text-right">{job.date}</p>
                 </div>
               </div>
             ))}
