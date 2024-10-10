9273bdff4613f1036a5f6199791d600e252ee920
fisayo-dev <olufisayobadina@gmail.com>
2024-10-10 15:09:39 +0100

    exported pages

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,2 +1,6 @@
 import Home from './Home'
-export { Home }
+import Notifications from './Notifications'
+import Messages from './Messages'
+import Profile from './Profile'
+import Companies from './Companies'
+export { Home, Notifications, Messages, Companies, Profile }
