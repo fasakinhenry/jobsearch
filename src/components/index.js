b40a64e6f891bae5c4c2a26d0e666717de178ac5
fisayo-dev <olufisayobadina@gmail.com>
2024-10-10 09:10:38 +0100

    export TopBar component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Header from './Header';
 import Button from './Button';
 import Hero from './Hero';
 import Footer from './Footer';
+import TopBar from './TopBar';
 
-export { Header, Button, Hero, Footer };
+export { Header, Button, Hero, Footer, TopBar };
