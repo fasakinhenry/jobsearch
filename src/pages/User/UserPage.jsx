4bacf2148239ef99ed2101bc6501a4b82026bc7a
fisayo-dev <olufisayobadina@gmail.com>
2024-10-10 18:14:28 +0100

    created user pages layout styles

--- src/pages/User/UserPage.jsx
+++ src/pages/User/UserPage.jsx
@@ -3,7 +3,11 @@ const UserPage = ({ children }) => {
   return (
     <div>
       <TopBar />
-      {children}
+      <div className="relative">
+        <div className="conatainer px-20 py-2">
+        {children}
+        </div>
+      </div>
     </div>
   );
 };
