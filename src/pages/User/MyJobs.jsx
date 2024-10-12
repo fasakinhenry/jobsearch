82af51bf695d69bdb1ce6f576326c024f869bab9
fisayo-dev <olufisayobadina@gmail.com>
2024-10-12 10:19:16 +0100

    modified my jobs page

--- src/pages/User/MyJobs.jsx
+++ src/pages/User/MyJobs.jsx
@@ -1,10 +1,21 @@
-import UserPage from "./UserPage"
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import { Button } from "../../components";
+import UserPage from "./UserPage";
+import { Link } from "react-router-dom";
 const MyJobs = () => {
   return (
-      <UserPage>
-          <h2 className="text-2xl">My Jobs</h2>
+    <UserPage>
+      <div className="flex justify-between items-center">
+        <h2 className="text-2xl">My Jobs</h2>
+        <Link to="/my-jobs/create">
+          <Button>
+            <PlusCircleIcon className="h-6 w-6" />
+            <p>Create a Job</p>
+          </Button>
+        </Link>
+      </div>
     </UserPage>
-  )
-}
+  );
+};
 
-export default MyJobs
+export default MyJobs;
