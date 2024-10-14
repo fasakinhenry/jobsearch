fe82cffac62781b699f9a0879509272817e3efc2
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-14 19:34:35 +0100

    feat: create index.js to export all components in a single file

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,1 +1,6 @@
+import Header from './header';
+import JobList from './JobList';
+import Sidebar from './Sidebar';
+import SuggestedJobs from './SuggestedJobs';
 
+export { Header, JobList, Sidebar, SuggestedJobs };
