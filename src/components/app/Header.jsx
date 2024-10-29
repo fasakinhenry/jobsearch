1b0011c8ed8733e0955214c5b2f8718814cefbb9
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-29 13:59:57 +0100

    feat: update the Header component
    Use Navlinks to handle routing
    Installed react router dom

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Bell, MessageCircle, Menu } from 'lucide-react';
 import { Icon } from '@iconify/react';
 
 const Header = ({ onMenuClick }) => {
+  const activeClassName = 'text-green-600 flex items-center';
+  const inactiveClassName =
+    'text-gray-600 flex items-center hover:text-green-600 transition-colors';
+
   return (
     <header className='bg-white border-b border-gray-200 p-4'>
       <div className='flex items-center justify-between'>
@@ -15,46 +20,69 @@ const Header = ({ onMenuClick }) => {
               icon='mingcute:group-line'
               className='w-8 h-8 text-green-600'
             />
-            <a
+            <NavLink
               to='/'
               className='text-xl md:text-2xl font-bold ml-2 text-green-600'
             >
               Joblier
-            </a>
+            </NavLink>
           </div>
         </div>
         <nav className='hidden md:block'>
           <ul className='flex space-x-6'>
             <li>
-              <a href='#' className='text-green-600 flex items-center'>
+              <NavLink
+                to='/'
+                className={({ isActive }) =>
+                  isActive ? activeClassName : inactiveClassName
+                }
+              >
                 <Icon icon='mingcute:home-4-fill' className='mr-2' />
                 Home
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a href='#' className='text-gray-600 flex items-center'>
+              <NavLink
+                to='/find-work'
+                className={({ isActive }) =>
+                  isActive ? activeClassName : inactiveClassName
+                }
+              >
                 <Icon icon='mingcute:search-3-line' className='mr-2' />
                 Find Jobs
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a href='#' className='text-gray-600 flex items-center'>
+              <NavLink
+                to='/companies'
+                className={({ isActive }) =>
+                  isActive ? activeClassName : inactiveClassName
+                }
+              >
                 <Icon icon='mingcute:building-1-fill' className='mr-2' />
                 Companies
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a href='#' className='text-gray-600 flex items-center'>
+              <NavLink
+                to='/community'
+                className={({ isActive }) =>
+                  isActive ? activeClassName : inactiveClassName
+                }
+              >
                 <Icon icon='mingcute:comment-2-line' className='mr-2' />
                 Community
-              </a>
+              </NavLink>
             </li>
           </ul>
         </nav>
         <div className='flex items-center space-x-4'>
           {/* <Bell className='text-gray-600' />
           <MessageCircle className='text-gray-600' /> */}
-          <Icon icon='mingcute:notification-newdot-line' className='text-gray-600' />
+          <Icon
+            icon='mingcute:notification-newdot-line'
+            className='text-gray-600'
+          />
           <Icon icon='mingcute:message-3-line' className='text-gray-600' />
           <div className='w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center'>
             <Icon icon='mingcute:user-2-line' className='text-gray-600' />
