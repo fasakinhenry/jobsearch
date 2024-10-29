02d5ff2710390d032d03a8080385e99878fe164e
fisayo-dev <olufisayobadina@gmail.com>
2024-10-29 20:56:16 +0100

    change joblier loading display content

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,8 +1,10 @@
-import { useContext, useState, createContext, useEffect } from 'react';
-import BarLoader from 'react-spinners/BarLoader';
-import Swal from 'sweetalert2';
-import { account } from '../appwrite/config';
-import { ID } from 'appwrite';
+import { useContext, useState, createContext, useEffect } from "react";
+import BarLoader from "react-spinners/BarLoader";
+import { NavLink } from "react-router-dom";
+import { Icon } from "@iconify/react";
+import Swal from "sweetalert2";
+import { account } from "../appwrite/config";
+import { ID } from "appwrite";
 
 const AuthContext = createContext();
 
@@ -28,19 +30,19 @@ export const AuthProvider = ({ children }) => {
       if (err.code == 401) {
         Swal.fire({
           toast: true,
-          icon: 'error',
-          text: 'Invalid username or Email',
+          icon: "error",
+          text: "Invalid username or Email",
           timer: 4000,
-          position: 'top',
+          position: "top",
           showConfirmButton: false,
         });
       } else {
         Swal.fire({
           toast: true,
-          icon: 'error',
-          text: 'Unable to Access Server',
+          icon: "error",
+          text: "Unable to Access Server",
           timer: 4000,
-          position: 'top',
+          position: "top",
           showConfirmButton: false,
         });
       }
@@ -73,18 +75,18 @@ export const AuthProvider = ({ children }) => {
       if (err.code == 409) {
         Swal.fire({
           toast: true,
-          text: 'Email Address has already been taken',
-          position: 'top',
-          icon: 'error',
+          text: "Email Address has already been taken",
+          position: "top",
+          icon: "error",
           showConfirmButton: false,
           timer: 4000,
         });
       } else {
         Swal.fire({
           toast: true,
-          text: 'Unable to reach server. Try again',
-          icon: 'error',
-          position: 'top',
+          text: "Unable to reach server. Try again",
+          icon: "error",
+          position: "top",
           showConfirmButton: false,
           timer: 2000,
         });
@@ -95,9 +97,9 @@ export const AuthProvider = ({ children }) => {
 
   const googleSignIn = async () => {
     account.createOAuth2Session(
-      'google',
-      'http://localhost:3000/home',
-      'http://localhost:3000/login'
+      "google",
+      "http://localhost:3000/home",
+      "http://localhost:3000/login"
     );
     const userDetails = account.get();
     setUser(userDetails);
@@ -106,13 +108,13 @@ export const AuthProvider = ({ children }) => {
   const deleteUser = async (userId) => {
     try {
       await Swal.fire({
-        icon: 'warning',
-        title: 'Delete Account ?',
-        text: 'Are you sure you want to delete your account.',
-        confirmButtonText: 'Yes, delete it',
-        confirmButtonColor: '#2563eb',
-        cancelButtonText: 'No',
-        cancelButtonColor: '#d33',
+        icon: "warning",
+        title: "Delete Account ?",
+        text: "Are you sure you want to delete your account.",
+        confirmButtonText: "Yes, delete it",
+        confirmButtonColor: "#2563eb",
+        cancelButtonText: "No",
+        cancelButtonColor: "#d33",
         showCancelButton: true,
       }).then((result) => {
         if (result.isConfirmed) {
@@ -121,12 +123,12 @@ export const AuthProvider = ({ children }) => {
         }
       });
     } catch (error) {
-      console.error('Failed to delete user:', error);
+      console.error("Failed to delete user:", error);
     }
   };
 
   const logoutUser = () => {
-    account.deleteSession('current');
+    account.deleteSession("current");
     setUser(null);
   };
 
@@ -150,10 +152,19 @@ export const AuthProvider = ({ children }) => {
   return (
     <AuthContext.Provider value={contextData}>
       {loading ? (
-        <div className='w-[100vw] h-[100vh] grid items-center justify-center bg-gray-50 overflow-hidden anmate-load'>
-          <div className='text-center app-text-color justify-items-center grid gap-4 mx-auto'>
-            <h2 className='text-3xl font-bold'>Joblier</h2>
-            <BarLoader loading={loading} />
+        <div className="w-[100vw] h-[100vh] grid items-center justify-center bg-gray-50 overflow-hidden anmate-load">
+          <div className="text-center app-text-color justify-items-center grid gap-4 mx-auto">
+            <div className="flex text-5xl gap-2 items-center">
+              <Icon
+                icon="mingcute:group-line"
+                className="w-15 h-15 text-green-600"
+              />
+              <div
+                className="font-bold ml-2 text-green-600"
+              >
+                Joblier
+              </div>
+            </div>
           </div>
         </div>
       ) : (
