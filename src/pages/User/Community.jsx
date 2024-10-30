65b12dad7f0ea32738af6bf4ed863bf8da375665
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-30 08:05:38 +0100

    feat: create the Community Page

---
+++ src/pages/User/Community.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import AuthLayout from '../layouts/AuthLayout';
+import Sidebar from '../components/community/Sidebar';
+import CommunityHome from '../components/community/CommunityHome';
+import Posts from '../components/community/Posts';
+
+const Community = () => {
+  return (
+    <AuthLayout>
+      <div className='flex h-full'>
+        <Sidebar />
+        <div className='flex-1 overflow-y-auto bg-gray-50'>
+          <Routes>
+            <Route path='/' element={<CommunityHome />} />
+            <Route path='/posts' element={<Posts />} />
+          </Routes>
+        </div>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Community;
